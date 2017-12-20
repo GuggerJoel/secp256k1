fd36263a51b1413f1383d83b72a9995b0417ad59
Joel Gugger <joel.gugger@rdlp.ch>
2017-12-20 21:31:36 +0100

    First impl of Paillier System

--- src/tests.c
+++ src/tests.c
@@ -4889,6 +4889,10 @@ void run_ecdsa_openssl(void) {
 # include "modules/recovery/tests_impl.h"
 #endif
 
+#ifdef ENABLE_MODULE_THRESHOLD
+# include "modules/threshold/paillier_tests.h"
+#endif
+
 int main(int argc, char **argv) {
     unsigned char seed16[16] = {0};
     unsigned char run32[32] = {0};
@@ -5007,6 +5011,11 @@ int main(int argc, char **argv) {
 #ifdef ENABLE_MODULE_RECOVERY
     /* ECDSA pubkey recovery tests */
     run_recovery_tests();
+#endif
+
+#ifdef ENABLE_MODULE_THRESHOLD
+    /* Paillier homomorphic system tests */
+    run_paillier_tests();
 #endif
 
     secp256k1_rand256(run32);
