ffb77570d42a222cfe0ed6c4730c45f039b13fe7
Joel Gugger <joel.gugger@rdlp.ch>
2018-01-30 18:19:48 +0100

    Minor update

--- src/modules/threshold/eczkp.h
+++ src/modules/threshold/eczkp.h
@@ -1,3 +1,9 @@
+/**********************************************************************
+ * Copyright (c) 2017 Joel Gugger                                     *
+ * Distributed under the MIT software license, see the accompanying   *
+ * file COPYING or http://www.opensource.org/licenses/mit-license.php.*
+ **********************************************************************/
+
 #ifndef SECP256K1_ECZKP_H
 #define SECP256K1_ECZKP_H
 
